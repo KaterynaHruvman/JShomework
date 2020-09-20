bfb76eee5a5f6eda5436c3b5df327b0178cdc9c4
KaterynaHruvman <66679134+KaterynaHruvman@users.noreply.github.com>
2020-09-20 21:48:31 +0300

    2

---
+++ script1.js
@@ -0,0 +1,6 @@
+const invoice = 100;
+const stock = 100;
+
+// Write code under this line
+const message = stock >= invoice ? "Заказ оформлен, с вами свяжеться менеджер" : "На селаде недостаточно товаров!";
+
